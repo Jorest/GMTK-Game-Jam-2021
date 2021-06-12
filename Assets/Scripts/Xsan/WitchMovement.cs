9060f0cf54b2396b9784c16e4215c4f735b96b02
Imilkowski <58135969+Imilkowski@users.noreply.github.com>
2021-06-12 08:35:17 +0200

    Added a Witch

---
+++ Assets/Scripts/Xsan/WitchMovement.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WitchMovement : MonoBehaviour
+{
+    public float normalMoveSpeed;
+    public Sprite frontSprite;
+    public Sprite backSprite;
+
+    [HideInInspector]
+    public float moveSpeed;
+
+    [Header("Other")]
+
+    private Vector2 moveDirection;
+
+    private Rigidbody2D body;
+    private Transform spriteTransform;
+    private SpriteRenderer spriteRenderer;
+
+    private IEnumerator FloatAnimation()
+    {
+        float spriteYPos = 0f;
+        float amplitude = 0.01f;
+        int sideMultiplier = 1;
+        float speed = 0.02f;
+
+        while (true)
+        {
+            if (spriteYPos > amplitude)
+            {
+                sideMultiplier = -1;
+            }
+            else if (spriteYPos < -amplitude)
+            {
+                sideMultiplier = 1;
+            }
+
+            spriteYPos += speed * sideMultiplier * Time.deltaTime;
+
+            spriteTransform.position += new Vector3(0, spriteYPos, 0);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        spriteTransform = transform.GetChild(0);
+        spriteRenderer = spriteTransform.GetComponent<SpriteRenderer>();
+
+        moveSpeed = normalMoveSpeed;
+
+        StartCoroutine(FloatAnimation());
+    }
+
+    void Update()
+    {
+        moveDirection.x = Input.GetAxis("Horizontal");
+        moveDirection.y = Input.GetAxis("Vertical");
+
+        if (moveDirection.magnitude > 0.1f)
+        {
+            if (moveDirection.x > 0)
+            {
+                spriteRenderer.flipX = false;
+            }
+            else
+            {
+                spriteRenderer.flipX = true;
+            }
+
+            if (moveDirection.y > 0)
+            {
+                spriteRenderer.sprite = backSprite;
+            }
+            else
+            {
+                spriteRenderer.sprite = frontSprite;
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if(moveDirection.magnitude > 1)
+        {
+            moveDirection = moveDirection.normalized;
+        }
+
+        body.MovePosition(body.position + moveDirection * moveSpeed / 1000);
+    }
+}
