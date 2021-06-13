68f0a6b21eccc275420a712d97d000b42a7a692c
jorest <jorest1994@gmail.com>
2021-06-13 08:56:45 +0200

    test secene change

---
+++ Assets/Scripts/Jorest/Item.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public float cooldown = 60.0f;
+    private bool toggled = false;
+    public GameObject SnackPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (toggled == true)
+        {
+            cooldown -= Time.deltaTime;
+        }
+            
+            
+          
+        if (cooldown <= 0.0f)
+        {
+            toggled = false;
+        }
+        
+        
+        if (Input.GetKeyDown("Jump"))
+        {
+            toggled = true;
+            GameObject newSnack = Instantiate(SnackPrefab, transform.position, Quaternion.identity);
+        }
+
+    }
+}
