4877e8c6c7daf2625a8611dabec27876729d7f03
jorest <jorest1994@gmail.com>
2021-06-13 16:02:40 +0200

    menu

---
+++ Assets/Scripts/Jorest/GoToSceneName.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToSceneName : MonoBehaviour
+{
+  
+    public void goToSceneName(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+}
