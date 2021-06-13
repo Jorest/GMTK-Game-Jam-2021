2722cff25ae6ff5b07f89111396ed959b4924b74
jorest <jorest1994@gmail.com>
2021-06-13 12:29:22 +0200

    music system and item

---
+++ Assets/Scripts/Jorest/WitchColsouds.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WitchColsouds : MonoBehaviour
+{
+
+    // alien
+    // skeleton
+    // zombie
+    // demon
+    // other
+    public AudioClip alien;
+    public AudioClip skeleton;
+    public AudioClip zombie;
+    public AudioClip demon;
+    public AudioClip other;
+
+
+
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+
+        if (col.gameObject.tag == "Monster")
+        {
+            
+            GameObject monster = col.gameObject;
+            
+            string type = monster.GetComponent<MonsterType>().type;
+
+            switch (type)
+            {
+                case "zombie":
+                    AudioManager.Instance.PlaySFX(zombie);
+                    break;
+                case "alien":
+                    AudioManager.Instance.PlaySFX(alien);
+                    break;
+                case "skeleton":
+                    AudioManager.Instance.PlaySFX(skeleton);
+                    break;
+                case "demon":
+                    AudioManager.Instance.PlaySFX(demon);
+                    break;
+                case "other":
+                    AudioManager.Instance.PlaySFX(other);
+                    break;
+            }
+
+        }
+
+
+    }
+}
